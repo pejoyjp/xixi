312a5e6b0f1a5fab5a524f1076b4ee982bd71ebc
jin <jinpeng8111765@gmail.com>
2026-03-05 10:50:15 +0800

    Update default skillsRepo URL to pejoyjp/xixi-skills

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_DEPTS = ["engineering", "ops", "sales"] as const;
-export const DEFAULT_SKILLS_REPO_URL = "git@github.com:<org>/xixi-skills.git";
+export const DEFAULT_SKILLS_REPO_URL = "git@github.com:pejoyjp/xixi-skills.git";
 export const XIXI_DIRNAME = ".xixi";
 export const INDEX_FILENAME = "index.json";
 export const CONFIG_FILENAME = "config.json";
